b4e37772208b5b5a841e5bfe833087848c2ffa84
Ahmed <ahmed.max7586@gmail.com>
2019-10-13 05:59:30 -0400

    done

--- src/server.js
+++ src/server.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { CLIENT_ORIGIN, PORT, NODE_ENV } = require('./config');
 const CatsRouter = require('./cats/CatsRouter');
 const DogsRouter = require('./dogs/DogsRouter');
 const AdoptersRouter = require('./adopters/AdoptersRouter');
-const {PORT} = require('./config');
+
 const app = express();
-app.use(cors());
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Petful API!');
+});
 
 app.use('/api/cats', CatsRouter);
 app.use('/api/dogs', DogsRouter);
 app.use('/api/adopters', AdoptersRouter);
 
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -26,6 +41,6 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT,()=>{
-  console.log('Serving on 8080');
+app.listen(PORT, () => {
+  console.log(`Serving listening at http://localhost:${PORT} `);
 });
